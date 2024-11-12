3d892fd7769ea2083dabb499e1678ac637fcd0e1
ksyksh01201 <ksyksh01201@naver.com>
2024-11-12 21:54:35 +0900

    Add files via upload

---
+++ Project1211_121/FileName11_12.cpp
@@ -0,0 +1,18 @@
+//#include <stdio.h>
+//
+//void main()
+//{
+//	printf("100+100");
+//	printf("\n");
+//	printf("%d", 100 + 100);
+//	printf("\n");
+//}
+#include <stdio.h>
+
+void main()
+{
+	printf("%d", 100);
+	printf("\n");
+	printf("%d %d", 100,200);
+	printf("\n");
+}
